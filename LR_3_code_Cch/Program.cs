3987a9340b2f6850c8b555135b0907b93a5ee813
sator <sator@SATOR-PC>
2016-12-03 13:31:50 +0200

    Добавьте файлы проекта.

---
+++ LR_3_code_Cch/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_3_code_Cch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
